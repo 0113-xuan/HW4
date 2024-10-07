8b169bc9137b48e82a8ce10d4edf4a4173adbb62
USER <USER@>
2024-10-07 13:24:43 +0800

    加入專案檔案。

---
+++ S1120344TourSurvey/Controllers/ShoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using S1120344TourSurvey.Models;
+
+namespace S1120344TourSurvey.Controllers
+{
+    public class ShoesController : Controller
+    {
+        private TourSurveyEntities db = new TourSurveyEntities();
+
+        // GET: Shoes
+        public ActionResult Index()
+        {
+            return View(db.Shoes.ToList());
+        }
+
+        // GET: Shoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shoe shoe = db.Shoes.Find(id);
+            if (shoe == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shoe);
+        }
+
+        // GET: Shoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Shoes/Create
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,brand,prize,size,gender")] Shoe shoe)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Shoes.Add(shoe);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(shoe);
+        }
+
+        // GET: Shoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shoe shoe = db.Shoes.Find(id);
+            if (shoe == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shoe);
+        }
+
+        // POST: Shoes/Edit/5
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,brand,prize,size,gender")] Shoe shoe)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(shoe).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(shoe);
+        }
+
+        // GET: Shoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shoe shoe = db.Shoes.Find(id);
+            if (shoe == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shoe);
+        }
+
+        // POST: Shoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Shoe shoe = db.Shoes.Find(id);
+            db.Shoes.Remove(shoe);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ S1120344TourSurvey/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="S1120344TourSurvey.MvcApplication" Language="C#" %>
